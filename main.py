a51b318e3cc8cc8fbe91be3bd521b8ede7f153d8
avern2024 <avern2020@icloud.com>
2024-10-06 01:59:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# "1st program"
+print('task1')
+print(9 ** 0.5 * 5)
+print()
+print('task2')
+# "2nd program"
+print(9.99 > 9.9 and 1000 != 1000.1)
+print()
+print('task3')
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+print()
+print('task4')
+# "4th program"
+print(int(float('123.456') * 10) % 1230)
